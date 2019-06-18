20c719a518ca739395287833577dcb498f050c1d
a.shkuropatov <a.shkuropatov.workbox@gmail.com>
2019-06-18 14:12:17 +0300

    up

---
+++ resources/views/parts/head.blade.php
@@ -0,0 +1,7 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>Newspaper</title>
+
+<!-- Styles -->
+<link href="/css/app.css" rel="stylesheet">
