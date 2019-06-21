9e52739077e196e6387fcbbdc620d2a52589765c
a.shkuropatov <a.shkuropatov.workbox@gmail.com>
2019-06-21 12:18:58 +0300

    admin page start

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en" dir="rtl">
+<head>
+  <meta charset="UTF-8">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <title>Admin panel</title>
+</head>
+<body>
+    @include('parts/header')
+    <div id="admin"></div>
+    @include('parts/footer')
+    <script src="/js/admin.js" charset="utf-8"></script>
+</body>
+</html>
