36ee698b1732fe68f9b53feedfdb2df3290f9c49
Lemontrash <Jakartasdedibiyb0ss3>
2019-06-25 16:38:33 +0300

    resolved a lot of shit (hopefully)

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,18 +2,23 @@
 
 
 namespace App\Http\Controllers;
-
-
+use App\Admins;
+use App\HappyBirthsday;
 use App\Hashtag;
 use App\HashtagPosts;
 use App\Post;
+use App\PostContent;
+use App\PostTitle;
+use App\PostVideo;
 use App\Survey;
 use App\SurveyAnswerVariant;
+use App\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Storage;
 class AdminController extends Controller
 {
-
+    protected $post;
     public function __construct()
     {
 //        $this->middleware('admin');
@@ -25,17 +30,42 @@ class AdminController extends Controller
     }
 
     public function createFullPost(Request $request){
-        //meta
-        $metaTitle = $request->get('meta-title');
-        $hashtags = $request->get('celebrities'); //array
-        $hot = $request->get('hot'); //true-false
-        $author = $request->get('author');
-        $date = $request->get('date');
-
-        //content
-        $content = $request->get('content');
-        $headerMeta = $this->createPostHeaderMeta($metaTitle, $hashtags, $hot, $author, $date);
-
+        $sections = $request->get('sections');
+        foreach ($sections as $key => $section) {
+            if ($section['type'] == 'metaTitle'){
+                $metaTitle  = $section['meta-title'];
+                $hashtags   = $section['celebrities']; //array
+                $hot        = $section['hot']; //true-false
+                $author     = $section['author'];
+                $date       = $section['date'];
+                $this->post = $this->createPostHeaderMeta($metaTitle, $hashtags, $hot, $author, $date);
+            }
+            elseif($section['type'] == 'content'){
+                $content = $section['content'];
+                $this->createPostAddContent($this->post->id, $content, $key);
+            }
+            elseif($section['type'] == 'title'){
+                $title = $section['title'];
+                $this->createPostAddTitle($this->post->id, $title, $key);
+            }
+            elseif($section['type'] == 'video'){
+                $url = $section['video'];
+                $this->createPostAddTitle($this->post->id, $url, $key);
+            }
+            elseif($section['type'] == 'survey'){
+                $title = $section['title'];
+                if ($section['right'] != null){
+                    $right = $section['right'];
+                }
+                $this->createPostAddSurvey($section['variants'], $title, $this->post->id, $key, $right);
+            }
+            elseif ($section['type'] == 'image'){
+                exit(0);
+            }
+            elseif ($section['type'] == 'imageWithText'){
+                exit(0);
+            }
+        }
     }
 
     public function createPostHeaderMeta($metaTitle, $hashtags, $hot, $author, $date){
@@ -54,43 +84,53 @@ class AdminController extends Controller
         return $post;
     }
 
-
-    public function createPostAllContent(){
-
+    public function createPostAddContent($postId, $content, $order){
+        PostContent::create([
+            'postId' =>  $postId,
+            'content' => $content,
+            'order' => $order
+        ]);
     }
-
     public function createPostAddImage(){
-
     }
 
-    public function createPostAddTitle(){
-
+    public function createPostAddTitle($postId, $title, $order){
+        PostTitle::create([
+            'postId' =>  $postId,
+            'titleText' => $title,
+            'order' => $order
+        ]);
     }
-
     public function createPostAddImageWithText(){
-
     }
 
-    public function createPostAddSurvey($variants, $title, $postId, $order){
-
+    public function createPostAddSurvey($variants, $title, $postId, $order, $right){
         $survey = Survey::create([
             'postId' => $postId,
             'authorId' => \Auth::id(),
             'order' => $order,
             'question' => $title
         ]);
-
         foreach ($variants as $key => $variant) {
+            $status = false;
+            if ($survey == $key){
+                $status = true;
+            }
             SurveyAnswerVariant::create([
                 'surveyId' => $survey->id,
                 'question' => $variant,
+                'right' => $status,
                 'order' => $key
             ]);
         }
     }
 
-    public function createPostAddVideo(){
-
+    public function createPostAddVideo($postId, $url, $order){
+        PostVideo::create([
+            'postId' =>  $postId,
+            'titleText' => $url,
+            'order' => $order
+        ]);
     }
 
     public function getAllHashtags(){
@@ -100,7 +140,6 @@ class AdminController extends Controller
             $hashtagArray[$key]['img'] = $hashtag->image;
             $hashtagArray[$key]['id'] = $hashtag->id;
         }
-
         return json_encode($hashtagArray);
     }
 
@@ -110,12 +149,10 @@ class AdminController extends Controller
     }
 
     public function addHashtag(Request $request){
-
-      $image = $request->file('image');
-      $name = time().'.'.$image->getClientOriginalExtension();
-      $destinationPath = public_path('/images/tag-images');
-      $image->move($destinationPath, $name);
-
+        $image = $request->file('image');
+        $name = time().'.'.$image->getClientOriginalExtension();
+        $destinationPath = public_path('/images/tag-images');
+        $image->move($destinationPath, $name);
         $hashtag = Hashtag::create([
             'text' => $request->get('text'),
             'image' => '/images/tag-images/'.$name,
@@ -124,53 +161,99 @@ class AdminController extends Controller
     }
 
     public function updateHashtag(Request $request){
-
-
-      $image = $request->file('image');
-      $text = $request->get('text');
-      $tag_id = $request->get('id');
-      if($image) {
-        $name = time().'.'.$image->getClientOriginalExtension();
-        $destinationPath = public_path('/images/tag-images');
-        $image->move($destinationPath, $name);
-        Hashtag::where('id', $tag_id)->update(['image' => '/images/tag-images/'.$name]);
-      }
-      if(!empty($text) && $text != null) {
-        Hashtag::where('id', $tag_id)->update(['text' => $text]);
-      }
+        $image = $request->file('image');
+        $text = $request->get('text');
+        $tag_id = $request->get('id');
+        if($image) {
+            $name = time().'.'.$image->getClientOriginalExtension();
+            $destinationPath = public_path('/images/tag-images');
+            $image->move($destinationPath, $name);
+            Hashtag::where('id', $tag_id)->update(['image' => '/images/tag-images/'.$name]);
+        }
+        if(!empty($text) && $text != null) {
+            Hashtag::where('id', $tag_id)->update(['text' => $text]);
+        }
         return $this->getAllHashtags();
     }
 
+    public function createHappyBirthday(Request $request){
+        $image = $request->file('image');
+        $name = time().'.'.$image->getClientOriginalExtension();
+        $destinationPath = public_path('/images/happyBirthday');
+        $image->move($destinationPath, $name);
+        HappyBirthsday::create([
+            'text' => $request->get('text'),
+            'img' => '/images/tag-images/'.$name,
+        ]);
+    }
 
-//    public function createSurvey(Request $request){
-//        $vartiants = $request->get('answerVariants');
-//        $title = $request->get('title');
-//        $postId = $request->get('postId');
-//        $order = $request->get('order');
-//
-//        $survey = Survey::create([
-//            'postId' => $postId,
-//            'authorId' => \Auth::id(),
-//            'order' => $order,
-//            'question' => $title
-//        ]);
-//
-//        foreach ($vartiants as $key => $vartiant) {
-//            SurveyAnswerVariant::create([
-//                'surveyId' => $survey->id,
-//                'question' => $vartiant,
-//                'order' => $key
-//            ]);
-//        }
-//    }
+    public function addNewComparison(Request $request){
+    }
 
     public function getAllSurveys(){
         $allSurveys = Survey::all();
-
         foreach ($allSurveys as $key => $survey) {
             $all[$key]['survey'] = $survey;
             $all[$key]['variants'] = $survey->getAllVariants()->orderBy('order');
         }
         return json_encode($all);
+    }
+
+    public function showAllAdmins(){
+        $adminIds = Admins::all();
+        foreach ($adminIds as $adminId) {
+            if (!empty(User::where('id', $adminId->id)->first())){
+                $admin = User::where('id', $adminId->id)->first();
+//                dd($admin);
+                $allAdmins[$admin->id]['email'] = $admin->email;
+                $allAdmins[$admin->id]['name']  = $admin->email;
+                $allAdmins[$admin->id]['name'] = $admin->name;
+                $allAdmins[$admin->id]['phone'] = $admin->phone;
+                $allAdmins[$admin->id]['password'] = 'нахуй он тут вообще нужен';
+                if ($admin->isOnline() == false){
+                    $allAdmins[$admin->id]['status'] = 'offline';
+                }else{
+                    $allAdmins[$admin->id]['status'] = 'online';
+                }
+            }
+        }
+        if (!empty($allAdmins)){
+//            dd($allAdmins);
+            return json_encode($allAdmins);
+        }else{
+            return json_encode([]);
+        }
+    }
+
+    public function editAdmin(Request $request){
+        $name   = $request->get('name');
+        $email  = $request->get('email');
+        $phone  = $request->get('phone');
+        $id     = $request->get('id');
+        $admin = User::find($id);
+        if (isset($name)){
+            $admin->name = $name;
+        }
+        if (isset($email)){
+            $admin->email = $email;
+        }
+        if (isset($phone)){
+            $admin->phone = $phone;
+        }
+        $admin->save();
+        return $this->showAllAdmins();
+    }
+
+    public function deleteAdmin(Request $request){
+        $id = $request->get('id');
+        Admins::find($id)->delete();
+        return $this->showAllAdmins();
+    }
+
+    public function makeUserAdmin(Request $request){
+        Admins::create([
+            'userId' => $request->get('id'),
+        ]);
+        return json_encode(['success' => true]);
     }
 }
