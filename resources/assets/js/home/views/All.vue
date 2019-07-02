e52289e9ae1894ce0642da0586ab67767845eba7
Alexey Motovilov <motowelove@gmail.com>
2019-07-02 11:38:00 +0300

    prepull commit

--- resources/assets/js/home/views/All.vue
+++ resources/assets/js/home/views/All.vue
@@ -1,7 +1,13 @@
 <template>
   <main>
     <div class="main-content">
-      qweqweqweqweqweq
+      <DefaultPost/>
+      <QuadPost/>
+      <DefaultPost/>
+      <DefaultPost/>
+      <QuadPost/>
+      <DefaultPost/>
+      <DefaultPost/>
     </div>
     <side-news/>
   </main>
@@ -9,10 +15,21 @@
 
 <script>
 import SideNews from './../components/SideNews.vue';
+import DefaultPost from './../components/all/DefaultPost.vue';
+import QuadPost from './../components/all/QuadPost.vue';
 
 export default {
   components: {
     SideNews,
+    DefaultPost,
+    QuadPost,
+  },
+  created() {
+    axios.post('/getAllPostsByHashtag', {
+      hashtag_id: 3,
+    }).then(res => {
+      console.log(res.data);
+    });
   }
 }
 </script>
@@ -21,12 +38,20 @@ export default {
 
 main {
   display: flex;
-  flex-direction: 'row';
+  flex-direction: row;
   justify-content: flex-start;
   align-items: flex-start;
   padding: 0 30px;
   .main-content {
     flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+    justify-content: flex-start;
+  }
+  @media(max-width: 992px) {
+    flex-direction: column;
+    padding: 0 10px;
   }
 }
 
