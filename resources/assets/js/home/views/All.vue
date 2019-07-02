87637e50e9481f7a7b6fe16c718d77b16e3faab4
miroslaw.shpak@gmail.com <bytesbay@icloud.com>
2019-07-02 15:35:41 +0300

    qweqwe

--- resources/assets/js/home/views/All.vue
+++ resources/assets/js/home/views/All.vue
@@ -1,13 +1,10 @@
 <template>
   <main>
     <div class="main-content">
-      <DefaultPost/>
-      <QuadPost/>
-      <DefaultPost/>
-      <DefaultPost/>
-      <QuadPost/>
-      <DefaultPost/>
-      <DefaultPost/>
+      <template v-for="(item, i) in data">
+        <DefaultPost v-if="!isQuad(i)"/>
+        <QuadPost v-else/>
+      </template>
     </div>
     <side-news/>
   </main>
@@ -19,17 +16,30 @@ import DefaultPost from './../components/all/DefaultPost.vue';
 import QuadPost from './../components/all/QuadPost.vue';
 
 export default {
+  data() {
+    return {
+      data: [],
+    };
+  },
   components: {
     SideNews,
     DefaultPost,
     QuadPost,
   },
+  methods: {
+    isQuad(index) {
+      return (index - 1) % 3 == 0;
+    },
+    sync() {
+      return axios.post('/getAllPostsByHashtag', {
+        hashtag_id: this.$route.params.id,
+      }).then(res => {
+        this.data = res.data;
+      });
+    }
+  },
   created() {
-    axios.post('/getAllPostsByHashtag', {
-      hashtag_id: this.$route.params.id,
-    }).then(res => {
-      console.log(res.data);
-    });
+    this.sync();
   }
 }
 </script>
