d9c88963f777cbe7b918cdc0a6fb85447a21df24
miroslaw.shpak@gmail.com <bytesbay@icloud.com>
2019-07-03 15:58:55 +0300

    Emoji and Share and Ads and Store and Modals

---
+++ resources/assets/js/home/components/modals/Copied.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="a-modal">
+    Link copied to your buffer!
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    setTimeout(() => {
+      this.$emit('close');
+    }, 500);
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.a-modal {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+}
+</style>
