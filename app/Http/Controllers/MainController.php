69b866ebfd7e13deaff7738add2dfb1e2d16ff3a
Lemontrash <Jakartasdedibiyb0ss3>
2019-07-04 11:46:08 +0300

    emoji fix

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -206,6 +206,7 @@ class MainController extends Controller
             foreach ($images as $image) {
                 $fullPost['sections'][$image->order]['type'] = 'image';
                 $fullPost['sections'][$image->order]['value'] = $image->url;
+                $fullPost['sections'][$image->order]['description'] = $image->description;
             }
         }
 
