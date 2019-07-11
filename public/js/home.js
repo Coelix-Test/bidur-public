612fe16c221ab7cd3b7ca3ac9daf93bff7ec0877
a.shkuropatov <a.shkuropatov.workbox@gmail.com>
2019-07-11 14:06:15 +0300

    Update home.js

--- public/js/home.js
+++ public/js/home.js
@@ -3696,7 +3696,6 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony default export */ __webpack_exports__["default"] = ({
   methods: {
     setDurations: function setDurations() {
-      console.log($('.bdConfetty'));
       $('.bdConfetty path').each(function (index) {
         var min = 2,
             max = 5;
