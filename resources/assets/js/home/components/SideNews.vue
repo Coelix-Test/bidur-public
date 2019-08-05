9a75a6a61e823627d801fe6af99799ff187658f6
a.shkuropatov <a.shkuropatov.workbox@gmail.com>
2019-08-05 10:33:51 +0300

    upd recent posts

--- resources/assets/js/home/components/SideNews.vue
+++ resources/assets/js/home/components/SideNews.vue
@@ -43,7 +43,7 @@ export default {
     SideNewsPost,
   },
   created() {
-    axios.post('/getRecentPosts').then(res => {
+    axios.post('/getRecentPosts', {postId: id}).then(res => {
       this.data = res.data;
     });
   },
