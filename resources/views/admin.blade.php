ff0878ce9378415cfa97785203752b2ee4f74591
miroslaw.shpak@gmail.com <bytesbay@icloud.com>
2019-08-10 03:36:51 +0300

    qweqwe

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -5,8 +5,8 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-  <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
+  <link href="/css/app.css" rel="stylesheet">
+  <link href="/css/admin.css" rel="stylesheet">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   <title>Admin panel</title>
 </head>
