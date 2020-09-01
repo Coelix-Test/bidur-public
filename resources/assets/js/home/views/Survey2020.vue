3e5b4d9fe220f2882767f35c00fc569404796aaa
Fayyaz Khan <52707173+realx4rd@users.noreply.github.com>
2020-09-01 13:13:22 +0500

    fixes

--- resources/assets/js/home/views/Survey2020.vue
+++ resources/assets/js/home/views/Survey2020.vue
@@ -13,9 +13,17 @@ export default {
 
 <style lang="scss" scoped>
 
-iframe {
-  width: 100%;
-  height: 1000px;
+@media screen and (min-width: 480px) {
+  iframe {
+    width: 100%;
+    height: 1000px;
+    }
+}
+@media screen and (max-width: 480px) {
+  iframe {
+    width: 100%;
+    height: 600px;
+    }
 }
 
 </style>
