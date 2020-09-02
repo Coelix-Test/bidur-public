ccc3468711f6600c9afa907d1d11be34608ff3e5
Fayyaz Khan <52707173+realx4rd@users.noreply.github.com>
2020-09-02 13:10:14 +0500

    update

--- resources/assets/js/home/views/Index.vue
+++ resources/assets/js/home/views/Index.vue
@@ -3,7 +3,7 @@
 
     <template v-if="$store.getters['main-page/loaded']">
       <!-- <top-section v-if="!$store.getters['main-page/isSunday']"/> -->
-      <a href="http://israelbidurmusic.co.il"
+      <a href="/2020"
         class="top-banner">
         <img src="/img/banners/music2.png" alt="">
       </a>
